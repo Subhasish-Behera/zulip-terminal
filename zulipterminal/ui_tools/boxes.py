398f3c5ea37c261b1ac982469d681f410257a728
SUBHASISH BEHERA <greendoor3000@gmail.com>
2023-01-04 19:56:28 +0530

    test_boxes: Changes made to tests to assert intended behaviour.

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -908,9 +908,7 @@ class WriteBox(urwid.Pile):
                     header.focus_col = self.FOCUS_HEADER_BOX_STREAM
             else:
                 self.model.controller.report_error(
-                    [
-                        " Recipient(s) can not be edited while editing a PM."
-                    ]
+                    [" Recipient(s) can not be edited while editing a PM."]
                 )
                 header.focus_col = self.FOCUS_HEADER_BOX_RECIPIENT
 
