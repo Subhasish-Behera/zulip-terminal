6971516f66ab9aed096d2abef937502acacf8fe1
SUBHASISH BEHERA <greendoor3000@gmail.com>
2023-01-06 08:00:29 -0700

    boxes: Changes made to diffrentiate if the writebox created is due to edit key or reply key. Added 2 field to class WriteBox to know if edit key is pressed or reply key is pressed

--- tests/ui_tools/test_boxes.py
+++ tests/ui_tools/test_boxes.py
@@ -1608,7 +1608,7 @@ class TestWriteBox:
                 "private",
                 True,
                 False,
-                "CONTAINER_MESSAGE",
+                "CONTAINER_HEADER",
                 "HEADER_BOX_RECIPIENT",
                 id="message_to_recipient_box",
             ),
