221fe740d65ada21195068fa06490d08d07d612e
Subhasish-Behera <greendoor300@gmail.com>
2023-03-26 15:27:24 +0530

    commit 11

--- zulipterminal/core.py
+++ zulipterminal/core.py
@@ -496,6 +496,7 @@ class Controller:
                 typing_phrase = " are typing"
             typing_text = [("footer_contrast", active_conversation_info + " ")]
             typing_text.append(("footer", typing_phrase + next(dots)))
+            print("typing_text", typing_text)
             # typing_text.append(("", typing_phrase + next(dots)))
             self.view.set_footer_text(typing_text)
             time.sleep(0.45)
