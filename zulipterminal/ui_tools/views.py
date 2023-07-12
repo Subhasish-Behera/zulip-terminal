c5eed28277cb383043699e44fa0c7d1a693ab6c5
Subhasish-Behera <greendoor3000@gmail.com>
2023-07-12 15:34:29 +0530

    commit 3

--- zulipterminal/ui_tools/views.py
+++ zulipterminal/ui_tools/views.py
@@ -429,11 +429,12 @@ class TopicsView(urwid.Frame):
         self.empty_search = False
 
     def _set_initial_focus(self) -> None:
-        if self.view.stream_topic_map[self.stream_button.stream_id] is not None:
+        saved_topic_state = self.view.stream_topic_map[self.stream_button.stream_id]
+        if saved_topic_state is not None:
             for i, topic in enumerate(self.log):
                 if (
                     topic.topic_name
-                    is self.view.stream_topic_map[self.stream_button.stream_id]
+                    is saved_topic_state
                 ):
                     self.list_box.focus_position = i
                     break
